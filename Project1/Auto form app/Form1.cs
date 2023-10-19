190fc19d044446c33414189d605411117c52f337
Fnau99 <nafikri.generali@gmail.com>
2023-10-19 11:46:09 +0200

    added constructor car

---
+++ Project1/Auto form app/Form1.cs
@@ -0,0 +1,17 @@
+namespace Auto_form_app
+{
+    public partial class Form1 : Form
+    {
+        //declare and initialize the object from the class car.
+        private Project1.Car Mycar = new Project1.Car();   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
