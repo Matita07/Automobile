b81680c254cbbd081eccc3b1713cfdddb9b4f71f
Matita07 <mattia.grassi0705@gmail.com>
2023-10-19 12:16:32 +0200

    Boh

--- Project1/FormCar/Form1.cs
+++ Project1/FormCar/Form1.cs
@@ -1,7 +1,11 @@
+using Project1;
+
 namespace FormCar
 {
     public partial class FormCar : Form
     {
+        public InheritCar myCar = new InheritCar(new Random().Next(50, 100), "Fiat", "Panda", "Blue", false, engine.Diesel, new Random().Next(50, 100));
+
         public FormCar()
         {
             InitializeComponent();
